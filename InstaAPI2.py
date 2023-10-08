1233a9b02a1b5b6efbf5c1459b972420a6255762
npvno <nicolas.painvin@gmail.com>
2023-10-08 16:09:00 -0400

    Push

--- InstaAPI2.py
+++ InstaAPI2.py
@@ -2,51 +2,13 @@ import os
 
 
 
-x="""<h1>Click the Button to Run Script</h1>
-
-<button id="refreshButton">Refresh</button>
-
-  <script>
-    document.getElementById('refreshButton').addEventListener('click', function() {
-      // Trigger the GitHub Actions workflow by making an API request
-      fetch('/.github/workflows/run_python_script.yml', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ref: 'main',  // Specify the branch to trigger the workflow
-        }),
-      })
-        .then(response => {
-          if (response.ok) {
-            alert('Script triggered.');
-          } else {
-            alert('Failed to trigger the script.');
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          alert('An error occurred while running the script. Please try again later.');
-        });
-    });
-  </script>"""
+x="""<h1>Click the Button to Run Script</h1>"""
 
 
-# Get the path to the script's directory
-script_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Define the filename (index.html)
-html_filename = "index.html"
-
-# Combine the script's directory and filename to create the full file path
-html_file_path = os.path.join(script_directory, html_filename)
-
-print(f"Current Directory: {script_directory}")
-print(f"HTML File Path: {html_file_path}")
+html_filename="index.html"
 
 # Create an HTML file for output
-with open(html_file_path, "w") as html_file:
+with open(html_filename, "w") as html_file:
     # Write the HTML header
     html_file.write("<html>\n\n<head>\n</head>\n\n<body>\n\n")
     html_file.write(f'{x}\n')
