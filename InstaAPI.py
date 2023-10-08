a42e5822ec58203b466ca47a106f7e0597ed51f1
npvno <nicolas.painvin@gmail.com>
2023-10-08 15:25:15 -0400

    Edited actions permissions and triggers

--- InstaAPI.py
+++ InstaAPI.py
@@ -55,6 +55,37 @@ print(top_posts_list)
 
 
 
+
+x="""<h1>Click the Button to Run Script</h1>
+
+<button id="refreshButton">Refresh</button>
+
+  <script>
+    document.getElementById('refreshButton').addEventListener('click', function() {
+      // Trigger the GitHub Actions workflow by making an API request
+      fetch('/.github/workflows/run_python_script.yml', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ref: 'main',  // Specify the branch to trigger the workflow
+        }),
+      })
+        .then(response => {
+          if (response.ok) {
+            alert('Script triggered.');
+          } else {
+            alert('Failed to trigger the script.');
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          alert('An error occurred while running the script. Please try again later.');
+        });
+    });
+  </script>"""
+
 # Get the current working directory where the script is running
 current_directory = os.getcwd()
 
@@ -64,7 +95,8 @@ html_filename = "index.html"
 # Combine the current directory and filename to create the full file path
 html_file_path = os.path.join(current_directory, html_filename)
 
-# Rest of your script...
+print(f"Current Directory: {current_directory}")
+print(f"HTML File Path: {html_file_path}")
 
 # Create an HTML file for output
 with open(html_file_path, "w") as html_file:
@@ -85,6 +117,7 @@ with open(html_file_path, "w") as html_file:
 
     # Write the HTML footer
     html_file.write('<script src="https://www.instagram.com/embed.js"></script>\n')
+    html_file.write(f'{x}\n')
     html_file.write("</body>\n</html>\n")
 html_file.close()
 print(f"HTML file '{html_filename}' generated successfully.")
