5e8195a1637c672839d16d49660ff7b3ffe3d70e
npvno <nicolas.painvin@gmail.com>
2023-10-21 19:06:05 -0400

    40

--- get_posts.py
+++ get_posts.py
@@ -30,7 +30,7 @@ def num_posts_downloaded_per_profile(num_profiles):
 
 def get_all_posts(selected_profile):
     posts_limit = num_posts_downloaded_per_profile(num_followers)
-    posts_limit = 20
+    posts_limit = 40
     posts_iterator = selected_profile.get_posts()
     posts=[]
     try:
