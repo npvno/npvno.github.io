35407984170a00feec5d41372b5b4d0f6efcb79d
npvno <nicolas.painvin@gmail.com>
2023-11-11 23:25:45 -0500

    amending js

--- postsTablesLoader.js
+++ postsTablesLoader.js
@@ -1,4 +1,3 @@
-
 // Load and parse the JSON data
 fetch('top_posts.json')
     .then(response => response.json())
@@ -6,48 +5,56 @@ fetch('top_posts.json')
         const postTables = document.getElementById('post-tables');
 
         // Iterate through the users and create HTML elements
-        data.posts.forEach(post => {
-            
+        for (let i = 0; i < data.posts.length; i += 3) {
+            // Create a new table for each row
             const postTable = document.createElement('table');
             postTable.style.width = '100%';
+
+            // Create a new table row for each group of three posts
             const postTableRow = document.createElement('tr');
 
-            
-            const postTableCell = document.createElement('td');
+            for (let j = i; j < i + 3 && j < data.posts.length; j++) {
+                // Create a new table cell for each post
+                const postTableCell = document.createElement('td');
 
-            // Create a span element to contain "Post ID:" and ${post}
-            const postTextSpan = document.createElement('span');
-            postTextSpan.style.display = 'inline'; // Set display to inline
-            postTextSpan.textContent = 'Post ID: ';
+                // Create a span element to contain "Post ID:" and ${post}
+                const postTextSpan = document.createElement('span');
+                postTextSpan.style.display = 'inline'; // Set display to inline
+                postTextSpan.textContent = 'Post ID: ';
 
-            // Create an anchor element for the clickable ${post}
-            const postLink = document.createElement('a');
-            postLink.href = `https://www.instagram.com/p/${post}`;
-            postLink.target = '_blank';
-            postLink.textContent = `${post}`;
+                // Create an anchor element for the clickable ${post}
+                const postLink = document.createElement('a');
+                postLink.href = `https://www.instagram.com/p/${data.posts[j]}`;
+                postLink.target = '_blank';
+                postLink.textContent = `${data.posts[j]}`;
 
-            // Create the blockquote element
-            const blockquote = document.createElement('blockquote');
-            blockquote.setAttribute('style', 'width: 300px;');
-            blockquote.setAttribute('class', 'instagram-media');
-            blockquote.setAttribute('data-instgrm-version', '14');
-            blockquote.setAttribute('omitscript', 'true');
+                // Create the blockquote element
+                const blockquote = document.createElement('blockquote');
+                blockquote.setAttribute('style', 'width: 300px;');
+                blockquote.setAttribute('class', 'instagram-media');
+                blockquote.setAttribute('data-instgrm-version', '14');
+                blockquote.setAttribute('omitscript', 'true');
 
-            const blockquoteLink = document.createElement('a');
-            blockquoteLink.href = `https://www.instagram.com/p/${post}/`;
-            blockquote.appendChild(blockquoteLink);
+                const blockquoteLink = document.createElement('a');
+                blockquoteLink.href = `https://www.instagram.com/p/${data.posts[j]}/`;
+                blockquote.appendChild(blockquoteLink);
 
-            // Append the postLink and blockquote to the postTableCell
-            postTextSpan.appendChild(postLink);
-            postTableCell.appendChild(postTextSpan);
-            postTableCell.appendChild(blockquote); // Append the blockquote element
-            postTableRow.appendChild(postTableCell); 
-            
+                // Append the postLink and blockquote to the postTableCell
+                postTextSpan.appendChild(postLink);
+                postTableCell.appendChild(postTextSpan);
+                postTableCell.appendChild(blockquote); // Append the blockquote element
 
-            postTables.appendChild(postTableRow);
-        });
+                // Append the postTableCell to the postTableRow
+                postTableRow.appendChild(postTableCell);
+            }
+
+            // Append the postTableRow to the postTable
+            postTable.appendChild(postTableRow);
+
+            // Append the postTable to the postTables
+            postTables.appendChild(postTable);
+        }
     })
-
     .catch(error => {
         console.error('Error loading JSON data:', error);
     });
